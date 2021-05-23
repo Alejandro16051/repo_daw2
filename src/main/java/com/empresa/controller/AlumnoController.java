94d4a07d87b96433fa079ea28fecff6dbb8da964
Alejandro16051 <ajhsalejandro@gmail.com>
2021-05-23 15:46:44 -0500

    ini2

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,17 @@ public class AlumnoController {
 	@PostMapping
 	public ResponseEntity<Alumno> registra(@RequestBody Alumno obj) {
 		log.info(">>>> registra <<<<" + obj.getIdAlumno());
+		Alumno objSalida = service.insertaActualizaAlumno(obj);
+		if (objSalida != null) {
+			return ResponseEntity.ok(objSalida);
+		} else {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+	@PutMapping
+	public ResponseEntity<Alumno> actualizar(@RequestBody Alumno obj) {
+		log.info(">>>> actualiza <<<<" + obj.getIdAlumno());
 		Alumno objSalida = service.insertaActualizaAlumno(obj);
 		if (objSalida != null) {
 			return ResponseEntity.ok(objSalida);
